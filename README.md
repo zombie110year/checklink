d1a7f3ff7314e043e652b3e539a04ec384b12e02
zombie110year <zombie110year@outlook.com>
2019-09-19 14:54:17 +0800

    :sparkles: main function to start program

--- README.md
+++ README.md
@@ -18,3 +18,9 @@ checklink --target md path
 
 如果链接被识别为 http 链接, 则通过 request 发送一个请求.
 如果是一个指向本地的链接, 则以 `path` 作为根目录测试此文件是否存在.
+
+## Sequence
+
+```
+(FileSystem) -> FileIter -> path -> MarkdownParser -> Link -> Checker -> Result -> Output
+```
---
+++ checklink/main.py
@@ -0,0 +1,40 @@
+from .workers import HTTPChecker, LocalChecker, FileIter, MarkdownParseWorker
+from queue import Queue
+from pathlib import Path
+
+
+def main(root: str, checkers: int = 32):
+    """start main processing
+
+    :param int checkers: amount of HTTPCheck, Other Threads hava fix value.
+    """
+    dirs = Queue()
+    files = Queue()
+    http_links = Queue()
+    local_links = Queue()
+    results = Queue()
+    dirs.put(Path(root))
+
+    file_iters = [FileIter(dirs, dirs, files) for _ in range(4)]
+    markdown_parsers = [MarkdownParseWorker(
+        files, http_links, local_links) for _ in range(4)]
+    http_checkers = [HTTPChecker(http_links, results) for _ in range(checkers)]
+    local_checkers = [LocalChecker(local_links, results) for _ in range(1)]
+
+    for i in file_iters:
+        i.start()
+    for i in markdown_parsers:
+        i.start()
+    for i in local_checkers:
+        i.start()
+    for i in http_checkers:
+        i.start()
+
+    for i in http_checkers:
+        i.join()
+    for i in local_checkers:
+        i.join()
+    for i in markdown_parsers:
+        i.join()
+    for i in file_iters:
+        i.join()
