565ec99c0fcbecaa4f7d82006bc9d58d0c05fa06
zombie110year <zombie110year@outlook.com>
2019-09-19 16:53:07 +0800

    :sparkles: add --version flag

--- checklink/cli.py
+++ checklink/cli.py
@@ -1,10 +1,12 @@
 from argparse import ArgumentParser
 
 from .main import main
+from . import __version__
 
 
 def argparser():
     parser = ArgumentParser("checklink")
+    parser.add_argument("--version", action="version", version=f"%(prog)s {__version__}")
     parser.add_argument("root", nargs="?", const=".", default=".")
     parser.add_argument("-j", dest="checkers", type=int, required=False, default=4)
     return parser
