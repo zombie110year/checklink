298fcdb4fe73f7bca7cebe1525b03043ad9fc580
zombie110year <zombie110year@outlook.com>
2019-09-19 15:17:24 +0800

    :sparkles: command line interface

---
+++ checklink/cli.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+
+from .main import main
+
+
+def argparser():
+    parser = ArgumentParser("checklink")
+    parser.add_argument("root")
+    parser.add_argument("-j", dest="checkers", type=int, required=False, default=32)
+    return parser
+
+
+def cli():
+    args = argparser().parse_args()
+    main(args.root, args.checkers)
